3527fa4417baadbe705c0bd90ad2f6eedcad7136
Anders Bjørn Nielsen <andersbjorn88@gmail.com>
2023-02-05 10:30:11 +0100

    Working front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { SsshDate } from './sssh-date';
 import { SsshDateService } from './sssh-date.service';
+import { timer } from 'rxjs';
 import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   constructor(private ssshDateService: SsshDateService) {}
-  parsedBase64Key = CryptoJS.enc.Base64.parse('QkFOQU5fQ0FLRV9NQU4xNg==');
-  title = 'Ssssh Date';
-  date: String | undefined;
+
+  title: String = 'Ssssh Date';
+  date: String = '';
+
+  private parsedBase64Key: CryptoJS.lib.WordArray = CryptoJS.enc.Base64.parse(
+    'QkFOQU5fQ0FLRV9NQU4xNg=='
+  );
 
   ngOnInit(): void {
+    console.log(this.parsedBase64Key);
+    this.autoGetNewDateAndResetTimer();
+  }
+
+  autoGetNewDateAndResetTimer(): void {
     this.ssshDateService.getDate().subscribe((d: SsshDate) => {
       this.date = CryptoJS.AES.decrypt(d.date, this.parsedBase64Key, {
         mode: CryptoJS.mode.ECB,
         padding: CryptoJS.pad.Pkcs7,
       }).toString(CryptoJS.enc.Utf8);
+      timer(Math.random() * 4000 + 1000).subscribe(() => {
+        this.autoGetNewDateAndResetTimer();
+      });
     });
   }
 }
--- src/app/sssh-date.service.ts
+++ src/app/sssh-date.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { SsshDate } from './sssh-date';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root',
@@ -12,21 +11,5 @@ export class SsshDateService {
 
   getDate(): Observable<SsshDate> {
     return this.httpClient.get<SsshDate>('http://localhost:8080/date');
-  }
-
-  decryptDate(obsDate: Observable<SsshDate>): String {
-    var date: String = '';
-    var parsedBase64Key = CryptoJS.enc.Base64.parse('QkFOQU5fQ0FLRV9NQU4xNg==');
-    console.log(parsedBase64Key);
-
-    obsDate.subscribe((d: SsshDate) => {
-      var decrypted = CryptoJS.AES.decrypt(d.date, parsedBase64Key, {
-        mode: CryptoJS.mode.ECB,
-        padding: CryptoJS.pad.Pkcs7,
-      });
-      console.log(decrypted.toString(CryptoJS.enc.Utf8));
-    });
-    console.log(date);
-    return date;
   }
 }
